9cc193615350f6dc5aff3b8d80af974b8e8a56e7
Artem Hatchenko <artem.hatchenko@automat-it.com>
2020-06-17 21:45:27 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,73 @@
+# Kubernetes Jobs Cleanup
+
+If your K8s cluster support alpha features, you can use [TTLAfterFinished](https://kubernetes.io/docs/concepts/workloads/controllers/ttlafterfinished/) option instead this image.
+
+## Describe
+This is source code from [Docker image](https://hub.docker.com/repository/docker/gatchenko/k8s-jobs-cleanup) which built on **Python 3.7 Alpine** image. The image is designed to run by **CronJob**, to clear Jobs in the status of "**completed**" and their pods.
+
+## Options
+| Option | Describe | Default value | Required |
+| ------ | ------ | ------ | ------ |
+| CLEANUP_TIMEOUT | The time in seconds after completion of a job before deleted  | 60 | No |
+| KUBERNETES_NAMESPACE | Kubernetes namespace | default | No |
+
+## Kubernetes RBAC
+```yaml
+---
+apiVersion: v1
+kind: ServiceAccount
+metadata:
+  name: jobs-cleanup
+automountServiceAccountToken: false
+---
+kind: ClusterRole
+apiVersion: rbac.authorization.k8s.io/v1
+metadata:
+  name: jobs-cleanup
+rules:
+- apiGroups: [""]
+  resources: ["pods"]
+  verbs: ["get", "watch", "list", "delete"]
+- apiGroups: ["batch", "extensions"]
+  resources: ["jobs"]
+  verbs: ["get", "list", "watch", "delete"]
+---
+kind: ClusterRoleBinding
+apiVersion: rbac.authorization.k8s.io/v1
+metadata:
+  name: jobs-cleanup
+subjects:
+- kind: ServiceAccount
+  name: jobs-cleanup
+  namespace: default
+roleRef:
+  kind: ClusterRole
+  name: jobs-cleanup
+  apiGroup: rbac.authorization.k8s.io
+```
+
+## CronJob
+```yaml
+apiVersion: batch/v1beta1
+kind: CronJob
+metadata:
+  name: jobs-cleanup
+  namespace: kube-system
+spec:
+  schedule: "*/1 * * * *"
+  jobTemplate:
+    spec:
+      template:
+        spec:
+          containers:
+          - name: jobs-cleanup
+            image: gatchenko/k8s-jobs-cleanup:latest
+            env:
+            - name: CLEANUP_TIMEOUT
+              value: "60"
+            - name: KUBERNETES_NAMESPACE
+              value: "default"
+          restartPolicy: OnFailure
+          serviceAccountName: jobs-cleanup
+          automountServiceAccountToken: true
+```
---
+++ jobs_cleanup.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python3
+
+import logging
+import yaml
+import sys
+import os
+import time
+from datetime import datetime
+from kubernetes import client, config, utils
+import kubernetes.client
+from kubernetes.client.rest import ApiException
+
+
+cleanupTimeout = os.environ.get('CLEANUP_TIMEOUT', '60')
+kubeNamespace = os.environ.get('KUBERNETES_NAMESPACE', 'default')
+dateFormat = "%Y-%m-%d %H:%M:%S"
+
+# Set logging
+logging.basicConfig(stream=sys.stdout, level=logging.INFO)
+
+# Setup K8 configs
+config.load_incluster_config()
+configuration = kubernetes.client.Configuration()
+api_instance = kubernetes.client.BatchV1Api(kubernetes.client.ApiClient(configuration))
+
+
+def kube_cleanup_finished_jobs(namespace='default', state='Finished'):
+    deleteoptions = client.V1DeleteOptions(propagation_policy='Background')
+    try: 
+        jobs = api_instance.list_namespaced_job(namespace,
+                                                pretty=True,
+                                                timeout_seconds=0)
+    except ApiException as e:
+        print("Exception when calling BatchV1Api->list_namespaced_job: %s\n" % e)
+    
+    for job in jobs.items:
+        logging.debug(job)
+        jobname = job.metadata.name
+        jobstatus = job.status.conditions
+        completionTime = job.status.completion_time
+        if job.status.succeeded == 1:
+            currentTime = datetime.utcnow().strftime(dateFormat)
+            currentTime = datetime.strptime(currentTime, dateFormat)
+
+            completionTime = completionTime.strftime(dateFormat)
+            completionTime = datetime.strptime(completionTime, dateFormat)
+
+            diffTime = (currentTime - completionTime).total_seconds()
+
+            if int(diffTime) >= int(cleanupTimeout):
+                logging.info("Cleaning up Job: {}. Finished at: {}".format(jobname, job.status.completion_time))
+                try: 
+                    api_response = api_instance.delete_namespaced_job(jobname,
+                                                                      namespace,
+                                                                      body = deleteoptions,
+                                                                      grace_period_seconds = 0)
+                    logging.debug(api_response)
+                except ApiException as e:
+                    print("Exception when calling BatchV1Api->delete_namespaced_job: %s\n" % e)
+
+        else:
+            if jobstatus is None and job.status.active == 1:
+                jobstatus = 'active'
+            logging.info("Job: {} not cleaned up. Current status: {}".format(jobname, jobstatus))
+
+    return 0
+    
+
+if __name__ == '__main__':
+    kube_cleanup_finished_jobs()
+    sys.exit(0)
